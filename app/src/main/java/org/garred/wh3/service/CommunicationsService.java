dcb147f2e58579f57d83d9315481034290f98f58
Dave Garred <dave.garred@gmail.com>
2015-12-07 19:19:11 -0800

    start work on v2

--- app/src/main/java/org/garred/wh3/service/CommunicationsService.java
+++ app/src/main/java/org/garred/wh3/service/CommunicationsService.java
@@ -1,7 +1,18 @@
 package org.garred.wh3.service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
+import org.garred.seah3.model.communicable.CommunicablePacket;
+import org.garred.seah3.model.communicable.Events1;
+import org.garred.seah3.model.communicable.Messages1;
+import org.garred.seah3.model.v2.CalendarDto;
 import org.garred.seah3.threads.MasterGet;
 import org.garred.seah3.threads.ThreadCallback;
 
@@ -11,34 +22,65 @@ import android.os.Looper;
 import android.os.Handler.Callback;
 import android.os.Message;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 
 public class CommunicationsService implements ThreadCallback {
-	
+
+	public static final int COMPLETED_DOWNLOAD = 1000;
+	public static final int FAILED_DOWNLOAD = 1001;
+
 	private final Callback controllerCallback;
-	private MasterGet get = null;
+	private final ObjectMapper objectMapper;
+
+	public CalendarDto receivedDto;
 
 	public CommunicationsService(Callback callController) {
 		this.controllerCallback = callController;
+		objectMapper = new ObjectMapper();
 	}
 
 	public void downloadEventList(String androidId, int versionCode) {
 //		get = new MasterGet(this, "http://nullgeodesic.com/seahhh/v1?dev=" + androidId + "&vers=" + versionCode, "");
-		get = new MasterGet(this, "http://192.168.1.190:8080/seahhh/v1?dev=" + androidId + "&vers=" + versionCode, "");
-		new Thread(get).start();
+//		get = new MasterGet(this, "http://192.168.1.190:8080/seahhh/v1?dev=" + androidId + "&vers=" + versionCode, "");
+		Runnable runnable = getV2(this, androidId, versionCode);
+		new Thread(runnable).start();
 	}
 
 	@Override
 	public void update(int returnMessage) {
 		Message message = new Message();
 		Bundle bundle = new Bundle();
-		bundle.putSerializable("events", (Serializable) get.getEvents());
-		if(get.getMessage() != null && !get.getMessage().isEmpty()) {
-			bundle.putString("message", get.getMessage());
+		bundle.putSerializable("events", (Serializable) receivedDto.events);
+		if(receivedDto.message != null && !receivedDto.message.isEmpty()) {
+			bundle.putString("message", receivedDto.message);
 		}
 		message.setData(bundle);
 		Looper.prepare();
 		Handler handler = new Handler(controllerCallback);
 		handler.dispatchMessage(message);
+	}
+	
+	public Runnable getV2(final CommunicationsService threadCallback, String androidId, int versionCode) {
+		final String target = "http://nullgeodesic.com/seahhh/v2?dev=" + androidId + "&vers=" + versionCode;
+		return new Runnable() {
+			@Override
+			public void run() {
+				try {
+					URL url = new URL(target);
+					URLConnection connection = url.openConnection();
+					InputStream in = connection.getInputStream();
+					receivedDto = objectMapper.readValue(in, CalendarDto.class);
+					in.close();
+					threadCallback.update(COMPLETED_DOWNLOAD);
+				} catch (MalformedURLException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+					threadCallback.update(FAILED_DOWNLOAD);
+				}
+			}
+		};
 	}
 
 }
